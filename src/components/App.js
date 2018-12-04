8ab14e83619fda08e896d5cbd8b0b81611e0e5cb
Braden Shipley <bradenrshipley@gmail.com>
2018-12-04 15:34:41 -0600

    add complete functionality, bugtesting still needed

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,100 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
-import './App.css';
-
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
+import "./App.css"
+import Post from "./Post/Post"
+import Header from "./Header/Header"
+import Search from "./Header/Search/Search"
+import Compose from "./Compose/Compose"
+import axios from "axios"
 
 class App extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
-      posts: []
-    };
+      posts: [],
+      searchInput: ""
+    }
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this)
+    this.deletePost = this.deletePost.bind(this)
+    this.createPost = this.createPost.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.filterSearch = this.filterSearch.bind(this)
   }
-  
+
   componentDidMount() {
-
+    axios.get("https://practiceapi.devmountain.com/api/posts").then(res => {
+      this.setState({ posts: res.data })
+    })
   }
-
-  updatePost() {
   
+
+  updatePost(id, text) {
+    //updating id and text of post using Axios.put where ID matches the post.id, returning the state
+    axios
+      .put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text })
+      .then(res => {
+        this.setState({ posts: res.data })
+      })
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios
+      .delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+      .then(res => {
+        this.setState({ posts: res.data })
+      })
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios
+      .post(`https://practiceapi.devmountain.com/api/posts`, { text })
+      .then(res => {
+        this.setState({ posts: res.data })
+      })
+  }
+  filterSearch(e) {
+    axios
+      .get(
+        `https://practiceapi.devmountain.com/api/posts/filter?text=${encodeURI(
+          this.state.searchInput
+        )}`
+      )
+      .then(res => this.setState({ posts: res.data }))
+      // .catch()
+  }
+  handleChange(e) {
+    // console.log(e.target.value)
+    this.setState({ searchInput: e.target.value })
   }
 
   render() {
-    const { posts } = this.state;
-
+    const { posts } = this.state
+    console.log(posts)
     return (
       <div className="App__parent">
-        <Header />
+        <Header
+          filterSearchFn={this.filterSearch}
+          handleChange={this.handleChange}
+        />
 
         <section className="App__content">
-
-          <Compose />
-          
+          <Compose createPostFn={this.createPost} />
+          { posts.map(post => (
+            <Post
+              date={post.date}
+              key={post.id}
+              text={post.text}
+              updatePostFn={this.updatePost}
+              id={post.id}
+              deletePostFn={this.deletePost}
+            />
+          ))}
         </section>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
-import './Search.css';
+import "./Search.css"
 
-import SearchIcon from 'react-icons/lib/md/search';
+import SearchIcon from "react-icons/lib/md/search"
 
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+  
   render() {
     return (
       <section className="Search__parent">
-
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input placeholder="Search Your Feed" onChange={this.props.handleChange} />
 
-          <SearchIcon id="Search__icon" />
+          <SearchIcon id="Search__icon" onClick={this.props.filterSearchFn} />
         </div>
-        
       </section>
     )
   }
